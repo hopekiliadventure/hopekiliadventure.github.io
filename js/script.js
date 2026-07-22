3c0fb24297dc63c5a05083896f0458b3a8594833
hopekiliadventure <hopekiliadventure@gmail.com>
2026-07-22 15:11:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -151,3 +151,9 @@ function restartSlider(){
 }
 
 startSlider();
+const menuToggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
